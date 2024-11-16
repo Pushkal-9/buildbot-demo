badb82e6b4a8b06192d9e5eca8559b5089522da8
Pushkal-9 <61682783+Pushkal-9@users.noreply.github.com>
2024-11-15 22:52:25 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
-# Exit   immediately if a command exits with a non-zero status
+# Exit immediately if a command exits with a non-zero status
 set -e
 
 echo "Starting the build process..."
 
 # Navigate to the base directory of the project
 cd "$(dirname "$0")"
-
 # Clean and build the project using Maven
 echo "Running Maven clean and package..."
 mvn clean package
